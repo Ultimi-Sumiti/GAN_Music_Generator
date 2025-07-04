8b7eaf773a7f4d739d110530624359811165c573
Ale10chine <alex10.chinello@gmail.com>
2025-07-04 19:27:07 +0200

    Solved the dimension problem among tensor in the basic net and start to implement the Lightiningh module

--- models/model_v1.py
+++ models/model_v1.py
@@ -21,19 +21,25 @@ class Generator(nn.Module):
             nn.Linear(in_features=1024, out_features=512),
             nn.ReLU(),
             
-            # Reshape torch.rand(N, 512) --> torch.rand(N, 1, 2 , 256)
-            nn.Unflatten(1, (1, 2, 256))
+            # nn.Unflatten(1, (1, 256, 2)) 
+           
+            # Compressed layer
+            nn.Linear(in_features=512, out_features=2),
+            nn.ReLU(), 
+            # Reshape torch.rand(N, 2) --> torch.rand(N, 1 , 1 , 2)
+            nn.Unflatten(1,(1 , 1 , 2))
+
         )
 
         self.transp_conv_net = nn.Sequential(
             # Default: padding=0, output_padding=0,  dilation=1
-            nn.ConvTranspose1d(in_channels=1, out_channels=256, kernel_size=(1,2), stride=2),
+            nn.ConvTranspose2d(in_channels=1, out_channels=256, kernel_size=(1,2), stride=2),
             nn.ReLU(),
-            nn.ConvTranspose1d(in_channels=256, out_channels=256, kernel_size=(1,2), stride=2),
+            nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=(1,2), stride=2),
             nn.ReLU(),
-            nn.ConvTranspose1d(in_channels=256, out_channels=256, kernel_size=(1,2), stride=2),
+            nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=(1,2), stride=2),
             nn.ReLU(),
-            nn.ConvTranspose1d(in_channels=256, out_channels=1, kernel_size=(128,1), stride=1),
+            nn.ConvTranspose2d(in_channels=256, out_channels=1, kernel_size=(128,1), stride=1),
             nn.ReLU()
         )
         # Function f() to create a monophonic layer by prev. feature map, i.e
@@ -46,6 +52,7 @@ class Generator(nn.Module):
             y = self.fc_net(x)
             print(y.shape)
             y = self.transp_conv_net(y)
+            print(y.shape)
             return y
         
 # Discriminator Architecture
@@ -55,9 +62,9 @@ class Discriminator(nn.Module):
 
         self.conv_net = nn.Sequential(
             # Default: padding=0,  dilation=1
-            nn.Conv1d(in_channels=1, out_channels=14, kernel_size=(128,2), stride=2),
+            nn.Conv2d(in_channels=1, out_channels=14, kernel_size=(128,2), stride=2),
             nn.ReLU(),
-            nn.Conv1d(in_channels=14, out_channels=77, kernel_size=(1,4), stride=2),
+            nn.Conv2d(in_channels=14, out_channels=77, kernel_size=(1,4), stride=2),
             nn.ReLU(),
 
             # Reshape torch.rand(N, 77, 1 ,3) --> torch.rand(N, 231) 
@@ -78,6 +85,71 @@ class Discriminator(nn.Module):
         y = self.fc_net(y)
         return y
 
+  
+
+# Merging all togheter to expolit, building the entire GAN architechture with the lightining module
+# As function of this class we can directly implement the training process
+class GAN(L.LightningModule):
+    # Constructor
+    def __init__(
+        self,
+        # Dim of the noise vector used as input in the generator
+        latent_dim: int = 100,
+        # Image output shape: 
+        #channels,
+        #width,
+        #height,
+
+        # Learning rate (to tune) 
+        lr: float = 0.0002,
+        # Adam optimizer params (to tune)
+        b1: float = 0.5,
+        b2: float = 0.999,
+
+        # Minibatch size
+        batch_size: int = BATCH_SIZE,
+        **kwargs,
+    ):
+        # Heridarety: to initialize correctly the superclass 
+        super().__init__()
+
+        # Function to save all the hyperpar passed in the above constructor
+        self.save_hyperparameters()
+        
+        # To control manually the optimization step, since in GAN we have to use criteria
+        # in optimizing both Generator and discriminator
+        self.automatic_optimization = False
+
+        # Newtorks definition(Generator + Discriminator)
+        # data_shape = (channels, width, height)
+        self.generator = Generator(latent_dim)
+        self.discriminator = Discriminator()
+
+        #
+        self.validation_z = torch.randn(8, self.hparams.latent_dim)
+        self.example_input_array = torch.zeros(2, self.hparams.latent_dim)
+
+    # Forward step computed     
+    def forward(self):
+        return
+
+    # Loss of the Adversarial game 
+    def adversarial_loss(self,):
+        return
+
+    # Gan training algorithm    
+    def training_step(self, ) :
+        return 
+
+    def validation_step(self, ) :
+        return 
+
+    def configure_optimizers(self) :
+        return 
+    
+    def on_validation_epoch_end(self) :
+        return 
+        
 
 # Test Main
 if __name__== "__main__":
@@ -88,5 +160,5 @@ if __name__== "__main__":
     x = torch.rand(1, 100)
     out = net_generator(x)
 
-    print(out.size())
+    print("Generator output ", out.size())
 
