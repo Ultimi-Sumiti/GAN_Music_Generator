87d81b22615e89664e9a12cee6f8baf082eeae3f
luca <lucapiai98@proton.me>
2025-07-21 15:47:55 +0200

    Added GPU dataset

--- utils/dataset_loader.py
+++ utils/dataset_loader.py
@@ -5,23 +5,9 @@ from torch.utils.data import Dataset, DataLoader
 import h5py
 import random
 import numpy as np
+ 
 
-# Random sampler from dataset.
-def random_batch_sampler(dataset: Dataset, size: int):
-    prevs = []
-    currs = []
-    for _ in range(size):
-        rnd_idx = random.randint(0, len(dataset)-1)
-        sample = dataset[rnd_idx]
-        prev, curr = sample
-        prevs.append(prev)
-        currs.append(curr)
-
-    batch = torch.from_numpy(np.array([prevs, currs]))
-    return batch
-    
-
-# Define the MaestroV3DataSet.
+# Define the MaestroV3DataSet - CPU version (no preload).
 class MaestroV3DataSet(Dataset):
 
     def __init__(self, file_path: str, mode: str = "single"):
@@ -61,19 +47,94 @@ class MaestroV3DataSet(Dataset):
                 return prev, curr
 
 
+# Define the MaestroV3DataSet - GPU version (with preload).
+# The entire dataset is entirely preloaded in the GPU.
+class MaestroV3DataSet_GPU(Dataset):
+    def __init__(self, file_path: str, mode: str = "single"):
+        self.mode = mode
+
+        # Open h5 file and save the dataset.
+        with h5py.File(file_path, 'r') as f:
+            dataset = f['x'][:]
+
+        # Preload dataset in GPU.
+        device = "cuda"
+
+        if mode == "single":
+            # from [N, 128, 16] to  [N, 1, 128, 16].
+            self.dataset = torch.tensor(data, dtype=torch.float32).unsqueeze(1)
+            # Move to GPU.
+            self.dataset = self.dataset.to(device)
+        else:
+            prev, curr = zip(*dataset)
+            self.dataset = [
+                (
+                    # from [128, 16] to [1, 128, 16] -> move to GPU.
+                    torch.tensor(p, dtype=torch.float32).unsqueeze(0).to(device),
+                    # from [128, 16] to [1, 128, 16] -> move to GPU.
+                    torch.tensor(c, dtype=torch.float32).unsqueeze(0).to(device)
+                )
+                for p, c in zip(prev, curr)
+            ]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx]
+
+
 # Define the meastro DataLoader.
 class MaestroV3DataModule(L.LightningDataModule):
 
-    def __init__(self, data_dir: str, batch_size: int = 32, mode: str = "single"):
+    def __init__(
+            self, 
+            data_dir: str,
+            batch_size: int = 32,
+            mode: str = "single",
+            num_workers: int = 0,
+            preload_gpu: bool = False
+        ):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.mode = mode
+        self.preload_gpu = preload_gpu
+        self.nw = num_workers
 
     def setup(self, stage: str):
-        self.dataset = MaestroV3DataSet(self.data_dir, self.mode)
+        if self.preload_gpu:
+            self.dataset = MaestroV3DataSet_GPU(self.data_dir, self.mode)
+        else:
+            self.dataset = MaestroV3DataSet(self.data_dir, self.mode)
 
     def train_dataloader(self):
-        nw = 9 # Shuld be tuned based on the CPU.
-        return DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True,
-                         num_workers=nw)
+        if self.preload_gpu:
+            dataloader = DataLoader(
+                self.dataset,
+                batch_size=self.batch_size,
+                shuffle=True
+            )
+        else:
+            dataloader = DataLoader(
+                self.dataset,
+                num_workers=self.nw,
+                batch_size=self.batch_size,
+                shuffle=True
+            )
+        return dataloader
+
+
+# Random sampler from dataset.
+def random_batch_sampler(dataset: Dataset, size: int):
+    prevs = []
+    currs = []
+    for _ in range(size):
+        rnd_idx = random.randint(0, len(dataset)-1)
+        sample = dataset[rnd_idx]
+        prev, curr = sample
+        prevs.append(prev)
+        currs.append(curr)
+
+    batch = torch.from_numpy(np.array([prevs, currs]))
+    return batch
