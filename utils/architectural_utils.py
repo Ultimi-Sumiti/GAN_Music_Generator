33177920b8818ffd39b6816b5300a83f11016cdb
luca <lucapiai98@proton.me>
2025-07-22 16:57:16 +0200

    Added function to compute the norm of the gradient

--- utils/architectural_utils.py
+++ utils/architectural_utils.py
@@ -5,6 +5,18 @@ from torch.nn.parameter import Parameter
 
 
 
+# Used to compute the norm of the gradient.
+def get_gradient_norm(model, norm_type=2):
+    total_norm = 0.0
+    for p in model.parameters():
+        if p.grad is not None:
+            param_norm = p.grad.detach().data.norm(norm_type)
+            total_norm += param_norm.item() ** norm_type
+    total_norm = total_norm ** (1. / norm_type)
+    return total_norm
+
+
+
 # Used to init the weights of Discriminator and Generator.
 def weights_init(m):
     classname = m.__class__.__name__
