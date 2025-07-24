ca870a30900d0807c19bf47de02bd837c498e4c1
Daedalus_02 <mattia.scantamburlo02@gmail.com>
2025-07-24 17:05:44 +0200

    some fiexes in the comments

--- data/create_dataset1.py
+++ data/create_dataset1.py
@@ -1,75 +1,78 @@
 import pretty_midi
 import os
 import h5py
-
 from midi_preprocessing import *
 
 
 ### GLOBAL VARIABLES ###
+
 # Define the number of cols per bar.
 COLS_PER_BAR = 16
 # Define the directory of the dataset.
-INPUT_DIR_PATH = "./raw/maestro-v3.0.0/2018"
+INPUT_DIR_PATH = "./raw/maestro-v3.0.0/2009"
 # Define the output directory path.
 OUT_DIR_PATH = "./preprocessed/maestro-v3.0.0/dataset1/"
 # Define output file name.
-OUT_FILE_NAME = "2018_50k_augmented.h5"
+OUT_FILE_NAME = "2009_50k_augmented.h5"
 
 
 def extract_samples(dir_path, samples):
+    """This function is used to extract samples from all the midi files found at
+    the given dir_path.
+
+    Arguments:
+        dir_path   The path of the directory with midi files.
+        samples    List where we place all the sample that we extract.
+    """
     # Iterate through.
     for filename in os.listdir(dir_path):
-    
+
         # Skip non-midi files.
         if not filename.endswith(".midi"):
             continue
-    
+
         # Open the midi file.
         midi = pretty_midi.PrettyMIDI(os.path.join(dir_path, filename))
-    
+
         # Define parameters.
         bar_duration = get_bar_duration(midi)
         fs = COLS_PER_BAR / bar_duration
-        
+
         # Create meoldy piano roll.
         melody_roll = extract_melody(midi, fs=fs)
-     
+
         # Binarize melody piano roll.
         melody_roll[melody_roll > 0] = 1
         melody_roll = melody_roll.astype(bool)
 
         # Normalize melody piano roll.
         melody_roll = normalize_melody_roll(melody_roll, lb=60, ub=83)
-    
+
         # Add zero padding to the end of the piano roll.
         zero_padding = COLS_PER_BAR - (melody_roll.shape[1] % COLS_PER_BAR)
-        melody_roll = np.pad(melody_roll, ((0, 0), (0, zero_padding)), mode='constant', constant_values=0)
+        melody_roll = np.pad(
+            melody_roll, ((0, 0), (0, zero_padding)), mode="constant", constant_values=0
+        )
 
         # Fill piano roll.
         melody_roll = fill_melody_pauses(melody_roll)
 
         print("Splitting sample ->", filename)
-    
+
         # Split into samples of size 128xCOLS_PER_BAR.
         splits = int(melody_roll.shape[1] / COLS_PER_BAR)
         for i in range(splits):
             tmp = i * COLS_PER_BAR
-            sample = melody_roll[:, tmp:tmp+COLS_PER_BAR]
+            sample = melody_roll[:, tmp : tmp + COLS_PER_BAR]
             samples.append(sample)
 
         print(f"\tSplitted in {splits} samples.")
-
-    
-    #print(len(samples))
-    #samples = np.stack(samples)
-    #with h5py.File("midi_dataset.h5", "w") as f:
-    #    f.create_dataset("midi_matrices", data=samples, compression="gzip")
 
 
 def main():
     # Store the samples in here.
     samples = []
-    
+
     extract_samples(INPUT_DIR_PATH, samples)
 
     print("Dataset has", len(samples), " samples")
